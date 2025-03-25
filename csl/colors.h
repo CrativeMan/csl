2b63101289454d5632a3c77dc7ebeae15915c438
Sebastian Hannig <hannig.sebi@gmail.com>
2025-03-25 09:22:25 +0100

    feat(colors): more colors

--- csl/colors.h
+++ csl/colors.h
@@ -3,8 +3,10 @@
 
 #define CSL_COLOR_RESET "\033[0m"
 #define CSL_COLOR_RED "\033[0;31m"
-#define CSL_COLOR_YELLOW "\033[0;33m"
 #define CSL_COLOR_GREEN "\033[0;32m"
+#define CSL_COLOR_YELLOW "\033[0;33m"
+#define CSL_COLOR_BLUE "\033[0;34m"
+#define CSL_COLOR_MAGENTA "\033[0;35m"
 #define CSL_COLOR_CYAN "\033[0;36m"
 
 #endif // !CSL_COLORS_H
