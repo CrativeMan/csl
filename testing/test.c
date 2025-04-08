25e07907bba4c46d8d86c671b6c95b6820cf1dda
Sebastian Hannig <hannig.sebi@gmail.com>
2025-04-08 21:53:07 +0200

    feat(datatypes): initial errorcodes

--- testing/test.c
+++ testing/test.c
@@ -1,4 +1,5 @@
 #include "../csl/colors.h"
+#include "../csl/datatypes/error.h"
 #include "../csl/datatypes/optional.h"
 #include "../csl/datatypes/pair.h"
 #include "../csl/datatypes/result.h"
@@ -25,7 +26,8 @@ void print_test_result(const char *test_name, int passed) {
   char *symbol = passed ? "✓" : "◯";
   char *color = passed ? CSL_COLOR_GREEN : CSL_COLOR_RED;
   char *context = passed ? "PASSED" : "FAILED";
-  printf("%d. %s %s: %s%s%s\n", testCount, symbol, test_name, color, context, CSL_COLOR_RESET);
+  printf("%d. %s %s: %s%s%s\n", testCount, symbol, test_name, color, context,
+         CSL_COLOR_RESET);
 }
 
 void test_vector_set() {
@@ -420,7 +422,8 @@ void test_string_builder() {
   {
     csl_sb *sb = csl_sb_init();
     int result = csl_sb_append(sb, "Hello, World");
-    int passed = (result == 1 && strcmp(sb->contents, "Hello, World") == 0 && sb->length == 12);
+    int passed = (result == 1 && strcmp(sb->contents, "Hello, World") == 0 &&
+                  sb->length == 12);
     print_test_result("csl_sb_append", passed);
     csl_sb_delete(sb);
   }
@@ -428,7 +431,8 @@ void test_string_builder() {
     csl_sb *sb = csl_sb_init();
     int result = csl_sb_append(sb, "Hello, World");
     csl_sb_clear(sb);
-    int passed = (result == 1 && strcmp(sb->contents, "") == 0 && sb->length == 0 && sb->contents[0] == '\0');
+    int passed = (result == 1 && strcmp(sb->contents, "") == 0 &&
+                  sb->length == 0 && sb->contents[0] == '\0');
     print_test_result("csl_sb_clear", passed);
     csl_sb_delete(sb);
   }
@@ -440,6 +444,13 @@ void test_string_builder() {
     print_test_result("csl_sb_to_string", passed);
     csl_sb_delete(sb);
     free(rb);
+  }
+}
+
+void test_error_codes() {
+  {
+    // TODO: Test this somehow lol
+    csl_ecode error = CSL_OK;
   }
 }
 
