7dcfc6c42640c44900182689b346d6dc73148a9c
CrativeMan <hannig.sebi@gmail.com>
2025-04-13 16:11:33 +0200

    feat(tests): Added more null ptr tests

--- TODO.md
+++ TODO.md
@@ -1,3 +1,2 @@
 * Testing
     * write null case tests for all other functions
-    * fix test `csl_v2i_add_vec_null` not being able to run
--- testing/test.c
+++ testing/test.c
@@ -146,10 +146,9 @@ void test_vector_add(void) {
         print_test_result("csl_v2i_add_vec", passed);
     }
     {
-        v2i vec1, vec2;
-        csl_v2i_add_vec(&vec1, &vec2);
-        int passed = (vec1.x == 0 && vec1.y == 0);
-        print_test_result("csl_v2i_add_vec_null", passed);
+        int passed = true;
+        csl_v2i_add_vec(NULL, NULL);
+        print_test_result("csl_v2i_add_vec_null_ptrs", passed);
     }
     {
         v2f vec1, vec2;
@@ -160,6 +159,11 @@ void test_vector_add(void) {
         print_test_result("csl_v2f_add_vec", passed);
     }
     {
+        int passed = true;
+        csl_v2f_add_vec(NULL, NULL);
+        print_test_result("csl_v2f_add_vec_null_ptrs", passed);
+    }
+    {
         v3i vec1, vec2;
         csl_v3i_set(&vec1, 5, 10, 15);
         csl_v3i_set(&vec2, 3, 4, 5);
@@ -168,12 +172,22 @@ void test_vector_add(void) {
         print_test_result("csl_v3i_add_vec", passed);
     }
     {
+        int passed = true;
+        csl_v3i_add_vec(NULL, NULL);
+        print_test_result("csl_v3i_add_vec_null_ptrs", passed);
+    }
+    {
         v3f vec1, vec2;
         csl_v3f_set(&vec1, 5.5f, 10.5f, 15.5f);
         csl_v3f_set(&vec2, 3.5f, 4.5f, 5.5f);
         csl_v3f_add_vec(&vec1, &vec2);
         int passed = (vec1.x == 9 && vec1.y == 15 && vec1.z == 21);
         print_test_result("csl_v3f_add_vec", passed);
+    }
+    {
+        int passed = true;
+        csl_v3f_add_vec(NULL, NULL);
+        print_test_result("csl_v3f_add_vec_null_ptrs", passed);
     }
 }
 
@@ -187,12 +201,22 @@ void test_vector_subtract(void) {
         print_test_result("csl_v2i_subtract", passed);
     }
     {
+        int passed = true;
+        csl_v2i_subtract(NULL, NULL);
+        print_test_result("csl_v2i_subtract_null_ptrs", passed);
+    }
+    {
         v2f vec1, vec2;
         csl_v2f_set(&vec1, 5.5f, 10.5f);
         csl_v2f_set(&vec2, 3.5f, 4.5f);
         csl_v2f_subtract(&vec1, &vec2);
         int passed = (vec1.x == 2 && vec1.y == 6);
         print_test_result("csl_v2f_subtract", passed);
+    }
+    {
+        int passed = true;
+        csl_v2f_subtract(NULL, NULL);
+        print_test_result("csl_v2f_subtract_null_ptrs", passed);
     }
     {
         v3i vec1, vec2;
@@ -203,12 +227,22 @@ void test_vector_subtract(void) {
         print_test_result("csl_v3i_subtract", passed);
     }
     {
+        int passed = true;
+        csl_v3i_subtract(NULL, NULL);
+        print_test_result("csl_v3i_subtract_null_ptrs", passed);
+    }
+    {
         v3f vec1, vec2;
         csl_v3f_set(&vec1, 5.5f, 10.5f, 15.5f);
         csl_v3f_set(&vec2, 3.5f, 4.5f, 5.5f);
         csl_v3f_subtract(&vec1, &vec2);
         int passed = (vec1.x == 2 && vec1.y == 6 && vec1.z == 10);
         print_test_result("csl_v3f_subtract", passed);
+    }
+    {
+        int passed = true;
+        csl_v3f_subtract(NULL, NULL);
+        print_test_result("csl_v3f_subtract_null_ptrs", passed);
     }
 }
 
@@ -222,12 +256,22 @@ void test_vector_dot(void) {
         print_test_result("csl_v2i_dot", passed);
     }
     {
+        i32 result = csl_v2i_dot(NULL, NULL);
+        int passed = (result == -1);
+        print_test_result("csl_v2i_dot_null_ptrs", passed);
+    }
+    {
         v2f vec1, vec2;
         csl_v2f_set(&vec1, 5.5f, 10.5f);
         csl_v2f_set(&vec2, 3.5f, 4.5f);
         f32 result = csl_v2f_dot(&vec1, &vec2);
         int passed = (result == 66.5f);
         print_test_result("csl_v2f_dot", passed);
+    }
+    {
+        f32 result = csl_v2f_dot(NULL, NULL);
+        int passed = (result == -1);
+        print_test_result("csl_v2f_dot_null_ptrs", passed);
     }
     {
         v3i vec1, vec2;
@@ -238,12 +282,22 @@ void test_vector_dot(void) {
         print_test_result("csl_v3i_dot", passed);
     }
     {
+        i32 result = csl_v3i_dot(NULL, NULL);
+        int passed = (result == -1);
+        print_test_result("csl_v3i_dot_null_ptrs", passed);
+    }
+    {
         v3f vec1, vec2;
         csl_v3f_set(&vec1, 5.5f, 10.5f, 15.5f);
         csl_v3f_set(&vec2, 3.5f, 4.5f, 5.5f);
         f32 result = csl_v3f_dot(&vec1, &vec2);
         int passed = (result == 151.75f);
         print_test_result("csl_v3f_dot", passed);
+    }
+    {
+        f32 result = csl_v3f_dot(NULL, NULL);
+        int passed = (result == -1);
+        print_test_result("csl_v3f_dot_null_ptrs", passed);
     }
 }
 
@@ -258,6 +312,11 @@ void test_vector_cross(void) {
         print_test_result("csl_v3i_cross", passed);
     }
     {
+        int passed = true;
+        csl_v3i_cross(NULL, NULL, NULL);
+        print_test_result("csl_v3i_cross_null_ptrs", passed);
+    }
+    {
         v3f vec1, vec2, result;
         csl_v3f_set(&vec1, 5.5f, 10.5f, 15.5f);
         csl_v3f_set(&vec2, 3.5f, 4.5f, 5.5f);
@@ -265,6 +324,11 @@ void test_vector_cross(void) {
         csl_v3f_cross(&result, &vec1, &vec2);
         int passed = (result.x == -12 && result.y == 24 && result.z == -12);
         print_test_result("csl_v3f_cross", passed);
+    }
+    {
+        int passed = true;
+        csl_v3f_cross(NULL, NULL, NULL);
+        print_test_result("csl_v3f_cross_null_ptrs", passed);
     }
 }
 
